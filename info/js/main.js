d6c4d0a5b02cf4bfc807bf17400d1e3255994f8b
FLORES FELIXBERTOJR. ABLITER <ffaa@mnlserv1.manship.com>
2018-01-05 03:14:57 +0000

    comment added index.html and info

---
+++ info/js/main.js
@@ -0,0 +1,219 @@
+/**
+ * @package Helix3 Framework
+ * @author JoomShaper http://www.joomshaper.com
+ * @copyright Copyright (c) 2010 - 2016 JoomShaper
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
+ */
+
+
+//Waypoints
+$(document).ready(function(){
+ 
+  // hide our element on page load
+  $('#img1').css('opacity', 0);
+ 
+  $('#img1').waypoint(function() {
+      $('#img1').addClass('fadeInLeft');
+  }, { offset: '80%' });
+
+  // hide our element on page load
+  $('#img2').css('opacity', 0);
+ 
+  $('#img2').waypoint(function() {
+      $('#img2').addClass('fadeInRight');
+  }, { offset: '80%' });
+
+  // hide our element on page load
+  $('#img3').css('opacity', 0);
+ 
+  $('#img3').waypoint(function() {
+      $('#img3').addClass('fadeInLeft');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img4').css('opacity', 0);
+
+  $('#img4').waypoint(function() {
+      $('#img4').addClass('fadeInRight');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img5').css('opacity', 0);
+ 
+  $('#img5').waypoint(function() {
+      $('#img5').addClass('fadeInLeft');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img6').css('opacity', 0);
+
+  $('#img6').waypoint(function() {
+      $('#img6').addClass('fadeInRight');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img7').css('opacity', 0);
+ 
+  $('#img7').waypoint(function() {
+      $('#img7').addClass('fadeInLeft');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img8').css('opacity', 0);
+
+  $('#img8').waypoint(function() {
+      $('#img8').addClass('fadeInRight');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img9').css('opacity', 0);
+ 
+  $('#img9').waypoint(function() {
+      $('#img9').addClass('fadeInLeft');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img10').css('opacity', 0);
+
+  $('#img10').waypoint(function() {
+      $('#img10').addClass('fadeInRight');
+  }, { offset: '80%' });
+
+    // hide our element on page load
+  $('#img11').css('opacity', 0);
+ 
+  $('#img11').waypoint(function() {
+      $('#img11').addClass('fadeInLeft');
+  }, { offset: '80%' });
+
+
+
+
+
+    // hide our element on page load
+  $('#imgb1').css('opacity', 0);
+
+  $('#imgb1').waypoint(function() {
+      $('#imgb1').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+      // hide our element on page load
+  $('#imgb2').css('opacity', 0);
+
+  $('#imgb2').waypoint(function() {
+      $('#imgb2').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+      // hide our element on page load
+  $('#imgb3').css('opacity', 0);
+
+  $('#imgb3').waypoint(function() {
+      $('#imgb3').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+      // hide our element on page load
+  $('#imgb4').css('opacity', 0);
+
+  $('#imgb4').waypoint(function() {
+      $('#imgb4').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+        // hide our element on page load
+  $('#imgb5').css('opacity', 0);
+
+  $('#imgb5').waypoint(function() {
+      $('#imgb5').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb6').css('opacity', 0);
+
+  $('#imgb6').waypoint(function() {
+      $('#imgb6').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb7').css('opacity', 0);
+
+  $('#imgb7').waypoint(function() {
+      $('#imgb7').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb8').css('opacity', 0);
+
+  $('#imgb8').waypoint(function() {
+      $('#imgb8').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb9').css('opacity', 0);
+
+  $('#imgb9').waypoint(function() {
+      $('#imgb9').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb10').css('opacity', 0);
+
+  $('#imgb10').waypoint(function() {
+      $('#imgb10').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb11').css('opacity', 0);
+
+  $('#imgb11').waypoint(function() {
+      $('#imgb11').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb12').css('opacity', 0);
+
+  $('#imgb12').waypoint(function() {
+      $('#imgb12').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb13').css('opacity', 0);
+
+  $('#imgb13').waypoint(function() {
+      $('#imgb13').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb14').css('opacity', 0);
+
+  $('#imgb14').waypoint(function() {
+      $('#imgb14').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb15').css('opacity', 0);
+
+  $('#imgb15').waypoint(function() {
+      $('#imgb15').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb16').css('opacity', 0);
+
+  $('#imgb16').waypoint(function() {
+      $('#imgb16').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+          // hide our element on page load
+  $('#imgb17').css('opacity', 0);
+
+  $('#imgb17').waypoint(function() {
+      $('#imgb17').addClass('fadeInUp');
+  }, { offset: '80%' });
+
+
+
+ 
+});
+
+
+
+
